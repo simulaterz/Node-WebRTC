09608a6aad2bcf6929d30325878227167cea7ec0
simulaterz <ronnakorn.suw@gmail.com>
2017-02-07 02:01:35 +0700

    setup port for testing on heroku

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const socketIO = require('socket.io');
 var app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
+var port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/public'));
 
@@ -36,6 +37,6 @@ io.on('connection', (socket) => {
     })
 });
 
-server.listen(3000, () => {
+server.listen(port, () => {
     console.log('Server started');
 });
