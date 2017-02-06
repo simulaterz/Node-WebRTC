e0cc4edfeb776a06bfc994de4cd3c7965c441b1a
simulaterz <ronnakorn.suw@gmail.com>
2017-02-07 01:55:31 +0700

    Setting up and test Signal

--- public/js/socket.js
+++ public/js/socket.js
@@ -2,11 +2,106 @@ const myName = document.querySelector('#myName');
 const myMessage = document.querySelector('#myMessage');
 const sendMessage = document.querySelector('#sendMessage');
 const chatArea = document.querySelector('#chatArea');
+const signalingArea = document.querySelector('#signalingArea');
+
 const ROOM = "chat";
+const SIGNAL_ROOM = "signal_room";
+
+var configuration = {
+    'iceServers': [{
+        'url': 'stun:stun.l.google.com:19302'
+    }]
+};
+var rtcPeerConn;
 
 var socket = io.connect();
 
-socket.emit('ready', ROOM);
+socket.emit('ready', {
+    chat_room: ROOM,
+    signal_room: SIGNAL_ROOM
+});
+
+socket.emit('signal', {
+    type: 'user_here',
+    message: 'Are you ready for a call?',
+    room: SIGNAL_ROOM
+});
+
+socket.on('signaling_message', (data) => {
+    displaySignalMessage(`Signal received: ${data.type}`);
+
+    if (!rtcPeerConn) {
+        startSignaling();
+    }
+
+    if (data.type != 'user_here') {
+        var message = JSON.parse(data.message);
+        if (message.sdp) {
+            rtcPeerConn.setRemoteDescription(new RTCSessionDescription(message.sdp), () => {
+                if (rtcPeerConn.remoteDescription.type == 'offer') {
+                    rtcPeerConn.createAnswer(sendLocalDesc, logError);
+                }
+            }, logError);
+        }
+        else {
+            rtcPeerConn.addIceCandidate(new RTCIceCandidate(message.candidate));
+        }
+    }
+});
+
+function startSignaling() {
+    displaySignalMessage("starting signaling...");
+
+    rtcPeerConn = new RTCPeerConnection(configuration);
+    rtcPeerConn.onicecandidate = (evt) => {
+        if (evt.candidate) {
+            socket.emit('signal', {
+                type: 'ice candidate',
+                message: JSON.stringify({
+                    candidate: evt.candidate
+                }),
+                room: SIGNAL_ROOM
+            });
+            displaySignalMessage("completed that ice candidate...");
+        }
+    };
+    rtcPeerConn.onnegotiationneeded = () => {
+        displaySignalMessage("on negotiation called");
+        rtcPeerConn.createOffer(sendLocalDesc, logError);
+    };
+    rtcPeerConn.onaddstream = (evt) => {
+        displaySignalMessage("going to add their stream...");
+        theirVideoArea.src = URL.createObjectURL(evt.stream);
+    };
+    navigator.mediaDevices.getUserMedia({
+        audio: true,
+        video: true
+    })
+        .then((stream) => {
+            displaySignalMessage("going to display my stream...");
+            myVideoArea.src = URL.createObjectURL(stream);
+            rtcPeerConn.addStream(stream);
+        })
+        .catch(logError);
+}
+
+function sendLocalDesc(desc) {
+    rtcPeerConn.setLocalDescription(desc, () => {
+        displaySignalMessage("sending local description");
+        socket.emit('signal', {
+            type: 'SDP',
+            message: JSON.stringify({
+                'sdp': rtcPeerConn.localDescription
+            }),
+            room: SIGNAL_ROOM
+        });
+    }, logError);
+}
+
+function logError(err) {
+    displaySignalMessage(`${err.name} : ${err.message}`);
+}
+
 
 socket.on('announce', (data) => {
     displayMessage(data.message);
@@ -27,4 +122,8 @@ sendMessage.addEventListener('click', (ev) => {
 
 function displayMessage(message) {
     chatArea.innerHTML = chatArea.innerHTML + "<br>" + message;
+}
+
+function displaySignalMessage(message) {
+    signalingArea.innerHTML = signalingArea.innerHTML + "<br>" + message;
 }
--- server.js
+++ server.js
@@ -14,11 +14,11 @@ app.get('/', (req, res) => {
 
 io.on('connection', (socket) => {
     socket.on('ready', (req) => {
-        roomName = req;
-        socket.join(roomName);
-        socket.broadcast.to(roomName).emit('announce', {
-            message: `New client in the ${roomName} room.`
-        });
+        socket.join(req.chat_room);
+        socket.join(req.signal_room);
+        // socket.broadcast.to(req.chat_room).emit('announce', {
+        //     message: `New client in the ${req.chat_room} room.`
+        // });
     });
 
     socket.on('send', (req) => {
@@ -27,6 +27,13 @@ io.on('connection', (socket) => {
             author: req.author
         });
     });
+
+    socket.on('signal', (req) => {
+        socket.broadcast.to(req.room).emit('signaling_message', {
+            type: req.type,
+            message: req.message
+        });
+    })
 });
 
 server.listen(3000, () => {
