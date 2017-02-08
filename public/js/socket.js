6cb51373c9b0033d4fd199940f8137435035a6be
simulaterz <ronnakorn.suw@gmail.com>
2017-02-08 16:05:41 +0700

    remake webRTC up to date

--- public/js/socket.js
+++ public/js/socket.js
@@ -7,14 +7,16 @@ const signalingArea = document.querySelector('#signalingArea');
 const ROOM = "chat";
 const SIGNAL_ROOM = "signal_room";
 
-// var configuration = {
-//     'iceServers': [{
-//         'url': 'stun:stun.l.google.com:19302'
-//     }]
-// };
-// var rtcPeerConn;
-
 var socket = io.connect();
+
+sendMessage.addEventListener('click', (ev) => {
+    socket.emit('send', {
+        room: ROOM,
+        message: myMessage.value,
+        author: myName.value,
+    });
+    ev.preventDefault();
+});
 
 socket.emit('ready', {
     chat_room: ROOM,
@@ -27,97 +29,12 @@ socket.emit('signal', {
     room: SIGNAL_ROOM
 });
 
-// socket.on('signaling_message', (data) => {
-//     displaySignalMessage(`Signal received: ${data.type}`);
-//
-//     if (!rtcPeerConn) {
-//         startSignaling();
-//     }
-//
-//     if (data.type != 'user_here') {
-//         var message = JSON.parse(data.message);
-//         if (message.sdp) {
-//             rtcPeerConn.setRemoteDescription(new RTCSessionDescription(message.sdp), () => {
-//                 if (rtcPeerConn.remoteDescription.type == 'offer') {
-//                     rtcPeerConn.createAnswer(sendLocalDesc, logError);
-//                 }
-//             }, logError);
-//         }
-//         else {
-//             rtcPeerConn.addIceCandidate(new RTCIceCandidate(message.candidate));
-//         }
-//     }
-// });
-//
-// function startSignaling() {
-//     displaySignalMessage("starting signaling...");
-//
-//     rtcPeerConn = new RTCPeerConnection(configuration);
-//     rtcPeerConn.onicecandidate = (evt) => {
-//         if (evt.candidate) {
-//             socket.emit('signal', {
-//                 type: 'ice candidate',
-//                 message: JSON.stringify({
-//                     candidate: evt.candidate
-//                 }),
-//                 room: SIGNAL_ROOM
-//             });
-//             displaySignalMessage("completed that ice candidate...");
-//         }
-//     };
-//     rtcPeerConn.onnegotiationneeded = () => {
-//         displaySignalMessage("on negotiation called");
-//         rtcPeerConn.createOffer(sendLocalDesc, logError);
-//     };
-//     rtcPeerConn.onaddstream = (evt) => {
-//         displaySignalMessage("going to add their stream...");
-//         theirVideoArea.src = URL.createObjectURL(evt.stream);
-//     };
-//     navigator.mediaDevices.getUserMedia({
-//         audio: true,
-//         video: true
-//     })
-//         .then((stream) => {
-//             displaySignalMessage("going to display my stream...");
-//             myVideoArea.src = URL.createObjectURL(stream);
-//             rtcPeerConn.addStream(stream);
-//         })
-//         .catch(logError);
-// }
-//
-// function sendLocalDesc(desc) {
-//     rtcPeerConn.setLocalDescription(desc, () => {
-//         displaySignalMessage("sending local description");
-//         socket.emit('signal', {
-//             type: 'SDP',
-//             message: JSON.stringify({
-//                 'sdp': rtcPeerConn.localDescription
-//             }),
-//             room: SIGNAL_ROOM
-//         });
-//     }, logError);
-// }
-//
-// function logError(err) {
-//     displaySignalMessage(`${err.name} : ${err.message}`);
-// }
-
-
 socket.on('announce', (data) => {
     displayMessage(data.message);
 });
 
 socket.on('message', (data) => {
     displayMessage(`${data.author} : ${data.message}`);
-});
-
-sendMessage.addEventListener('click', (ev) => {
-    socket.emit('send', {
-        room: ROOM,
-        message: myMessage.value,
-        author: myName.value,
-    });
-    ev.preventDefault();
 });
 
 function displayMessage(message) {
--- public/js/webrtc-test.js
+++ public/js/webrtc-test.js
@@ -1,61 +1,22 @@
 // url's'
 var configuration = {
-    'iceServers': [{
-        'urls': 'stun:stun.l.google.com:19302'
-    }]
+    "iceServers": [
+        {"urls": [
+            "stun:stun.l.google.com:19302",
+            "stun:stun1.l.google.com:19302",
+            "stun:stun2.l.google.com:19302",
+            "stun:stun3.l.google.com:19302",
+            "stun:stun4.l.google.com:19302"
+        ]}
+    ]
 };
 var rtcPeerConn;
 
-socket.on('signaling_message', (data) => {
-    displaySignalMessage(`Signal received: ${data.type}`);
-
-    if (!rtcPeerConn) {
-        startSignaling();
-    }
-
-    if (data.type != 'user_here') {
-        var message = JSON.parse(data.message);
-        if (message.sdp) {
-            var desc = message.sdp;
-
-            console.log(desc.type);
-            // if we get an offer, we need to reply with an answer
-            if (desc.type == "offer") {
-                displaySignalMessage("offer ...");
-                rtcPeerConn.setRemoteDescription(desc)
-                    .then(() => {
-                        return rtcPeerConn.createAnswer();
-                    })
-                    .then((answer) => {
-                        return rtcPeerConn.setLocalDescription(answer);
-                    })
-                    .then(() => {
-                        socket.emit('signal', {
-                            type: 'SDP',
-                            message: JSON.stringify({
-                                "sdp": rtcPeerConn.localDescription
-                            }),
-                            room: SIGNAL_ROOM
-                        });
-                    })
-                    .catch(logError);
-            } else if (desc.type == "answer") {
-                displaySignalMessage("answer ...");
-                rtcPeerConn.setRemoteDescription(desc).catch(logError);
-            } else {
-                log("Unsupported SDP type. Your code may differ here.");
-            }
-        } else {
-            rtcPeerConn.addIceCandidate(message.candidate).catch(logError);
-        }
-    }
-});
-
 function startSignaling() {
     displaySignalMessage("starting signaling...");
-    rtcPeerConn = new RTCPeerConnection(configuration);
 
-    rtcPeerConn.onicecandidate = (evt) => {
+    rtcPeerConn = new RTCPeerConnection(configuration);
+    rtcPeerConn.onicecandidate = function (evt) {
         if (evt.candidate) {
             socket.emit('signal', {
                 type: 'ice candidate',
@@ -68,14 +29,12 @@ function startSignaling() {
         }
     };
 
-    rtcPeerConn.onnegotiationneeded = () => {
-        rtcPeerConn.createOffer().then((offer) => {
+    rtcPeerConn.onnegotiationneeded = function () {
+        rtcPeerConn.createOffer().then(function (offer) {
             displaySignalMessage("on negotiation called");
-
             return rtcPeerConn.setLocalDescription(offer);
-        }).then(() => {
+        }).then(function () {
             displaySignalMessage("sending local description");
-
             socket.emit('signal', {
                 type: 'SDP',
                 message: JSON.stringify({"sdp": rtcPeerConn.localDescription}),
@@ -84,7 +43,7 @@ function startSignaling() {
         }).catch(logError);
     };
 
-    rtcPeerConn.ontrack = (evt) => {
+    rtcPeerConn.ontrack = function (evt) {
         if (evt.track.kind === "video") {
             displaySignalMessage("going to add their stream...");
             theirVideoArea.srcObject = evt.streams[0];
@@ -95,7 +54,7 @@ function startSignaling() {
         "audio": true,
         "video": true
     })
-        .then((stream) => {
+        .then(function (stream) {
             displaySignalMessage("going to display my stream...");
             myVideoArea.srcObject = stream;
             rtcPeerConn.addStream(stream);
@@ -103,7 +62,48 @@ function startSignaling() {
         .catch(logError);
 };
 
+socket.on('signaling_message', function (data) {
+    displaySignalMessage(`Signal received: ${data.type}`);
+
+    if (!rtcPeerConn) {
+        startSignaling();
+    }
+
+    if (data.type != 'user_here') {
+        var message = JSON.parse(data.message);
+        if (message.sdp) {
+            var desc = message.sdp;
+            if (desc.type == "offer") {
+                rtcPeerConn.setRemoteDescription(desc)
+                    .then(function () {
+                        return rtcPeerConn.createAnswer();
+                    })
+                    .then(function (answer) {
+                        return rtcPeerConn.setLocalDescription(answer);
+                    })
+                    .then(function () {
+                        socket.emit('signal', {
+                            type: 'SDP',
+                            message: JSON.stringify({
+                                "sdp": rtcPeerConn.localDescription
+                            }),
+                            room: SIGNAL_ROOM
+                        });
+                    })
+                    .catch(logError);
+            } else if (desc.type == "answer") {
+                rtcPeerConn.setRemoteDescription(desc)
+                    .catch(logError);
+            } else {
+                log("Unsupported SDP type. Your code may differ here.");
+            }
+        } else {
+            rtcPeerConn.addIceCandidate(message.candidate)
+                .catch(logError);
+        }
+    }
+});
+
 function logError(error) {
     displaySignalMessage(`${error.name} : ${error.message}`);
 }
-
