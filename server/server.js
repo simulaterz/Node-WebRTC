2f741fa8bc0fbad05fa1a54cb6ad9beeaf4f4ee3
simulaterz <ronnakorn.suw@gmail.com>
2017-03-06 02:00:51 +0700

    basic setup

--- server/server.js
+++ server/server.js
@@ -1,19 +1,18 @@
 const express = require('express');
 const http = require('http');
+const path = require('path');
+
+const publicPath = path.join(__dirname, '..', '/public');
 
 const port = process.env.PORT || 3000;
 
 const app = express();
 const server = http.createServer(app);
 
-app.use(express.static(__dirname + '/public'));
-
-app.get('/', (req, res) => {
-    res.render('index.ejs');
-});
+app.use(express.static(publicPath));
 
 server.listen(port, () => {
     console.log(`Server started on port ${port}`);
 });
 
-require('./Signaling-Server')(server);
+require('./modules/Signaling-Server')(server);
